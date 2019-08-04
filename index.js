0536533ee1451e1b0c23493ef3cd582d8d6667da
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:59:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Write your code here!
 
-/* Enter the code to remove the node element here */
+
 document.querySelector("main#main").remove();
 
-/* Create your new element here and assign it to newHeader */
+
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerHTML = "Sam is the champion!";
-// newHeader.className = "victory";
