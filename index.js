83eb80e30b6a0efe8d693f6d0dfc2bb141331baa
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ document.querySelector("main#main").remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
-// newHeader.id = "victory"
+newHeader.id = "victory"
 newHeader.innerHTML = "Sam is the champion!";
 // newHeader.className = "victory";
