ceb7a36a6ba437515afe579e3034271291aa2ece
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:58:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.innerHTML = "Sam is the champion!";
 newHeader.className = "victory";
