de4c28e2afd049fbe0d6dff1981b1c4570a43b2b
Samantha Kane <s.kane133@gmail.com>
2019-08-04 18:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here!\
-const var = "hi"
+const testVar = "hi"
 
 /* Enter the code to remove the node element here */
- document.querySelector("main#main").remove();
+document.querySelector("main#main").remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
